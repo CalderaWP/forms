de0a0a816a39eabd38632aa4d6d6ef7e65888240
Josh Pollock <josh@calderawp.com>
2018-12-21 11:37:41 -0500

    move FormModel to forms package

---
+++ src/FormModel.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace calderawp\caldera\Forms;
+
+
+use calderawp\interop\Caldera;
+use calderawp\interop\Contracts\CalderaForms\HasForm;
+use calderawp\interop\Contracts\CalderaForms\HasFields;
+use calderawp\interop\Traits\CalderaForms\ProvidesFields;
+use calderawp\interop\Traits\CalderaForms\ProvidesForm;
+use calderawp\interop\Contracts\CalderaContract as CalderaContract;
+
+class FormModel extends Caldera implements HasForm, HasFields, CalderaContract
+{
+	use ProvidesForm, ProvidesFields;
+}
+
---
+++ tests/FormModelTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace calderawp\caldera\Forms\Tests;
+
+use calderawp\caldera\Forms\FormModel;
+use calderawp\interop\Tests\TestCase;
+use calderawp\interop\Contracts\CalderaForms\HasField;
+use calderawp\interop\Contracts\CalderaForms\HasForm;
+use calderawp\interop\Tests\Traits\EntityFactory;
+
+class FormModelTest extends TestCase
+{
+
+	use EntityFactory;
+
+	/**
+	 * @covers FormModel::fromArray()
+	 */
+	public function testFromArray()
+	{
+		$form = $this->getForm();
+		$fields = $this->getFields();
+		$settings = $this->getSettings();
+		$model = FormModel::fromArray([
+			'form' => $form,
+			'fields' => $fields,
+			'settings' => $settings,
+		]);
+		$this->assertAttributeEquals($form, 'form', $model);
+		$this->assertAttributeEquals($fields, 'fields', $model);
+		$this->assertAttributeEquals($settings, 'settings', $model);
+	}
+
+	/**
+	 * @covers FormModel::toArray()
+	 * @covers \calderawp\interop\Traits\ConvertsInteropModelToArray::toArray()
+	 */
+	public function testToArray()
+	{
+		$form = $this->getForm();
+		$fields = $this->getFields();
+		$settings = $this->getSettings();
+		$model = FormModel::fromArray([
+			'form' => $form,
+			'fields' => $fields,
+			'settings' => $settings,
+		]);
+		$array = $model->toArray();
+		$this->assertEquals($form, $array[ 'form' ]);
+		$this->assertEquals($fields, $array[ 'fields' ]);
+		$this->assertEquals($settings, $array[ 'settings' ]);
+
+	}
+
+
+
+
+
+
+}
