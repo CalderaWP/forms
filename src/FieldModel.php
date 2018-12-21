910e2ad5d7358e1ad1c36ea72b2101cf39adc5b3
Josh Pollock <josh@calderawp.com>
2018-12-21 13:50:57 -0500

    field model

--- src/FieldModel.php
+++ src/FieldModel.php
@@ -4,14 +4,16 @@
 namespace calderawp\caldera\Forms;
 
 use calderawp\interop\Contracts\CalderaForms\HasField;
+use calderawp\interop\Contracts\HasValue;
 use calderawp\interop\Contracts\CalderaForms\HasForm;
 use calderawp\interop\Model;
 use calderawp\interop\Traits\CalderaForms\ProvidesField;
 use calderawp\interop\Traits\CalderaForms\ProvidesForm;
 use calderawp\interop\Traits\ProvidesId;
 use calderawp\interop\Traits\ProvidesName;
+use calderawp\interop\Traits\ProvidesValue;
 
-class FieldModel extends Model implements HasForm, HasField
+class FieldModel extends Model implements HasForm, HasField,HasValue
 {
-	use ProvidesField, ProvidesForm, ProvidesId, ProvidesName;
+	use ProvidesValue, ProvidesField, ProvidesForm, ProvidesId, ProvidesName;
 }
--- tests/FieldModelTest.php
+++ tests/FieldModelTest.php
@@ -3,8 +3,67 @@
 namespace calderawp\caldera\Forms\Tests;
 
 use calderawp\caldera\Forms\FieldModel;
+use calderawp\caldera\Forms\FormModel;
 
 class FieldModelTest extends TestCase
 {
+
+	public function testSetGetValue()
+	{
+		$field = new FieldModel();
+		$value = 'food';
+		$field->setValue($value);
+		$this->assertEquals($value, $field->getValue($value) );
+
+	}
+
+	public function testSetGetDefault()
+	{
+		$field = new FieldModel();
+		$default = 'drinks';
+		$value = 'food';
+		$field->setDefault($default);
+		$this->assertEquals($default, $field->getValue($default) );
+		$field->setValue($value);
+		$this->assertEquals($value, $field->getValue($value) );
+
+	}
+
+	public function testToArray()
+	{
+		$field = new FieldModel();
+		$default = 'drinks';
+		$value = 'food';
+		$id = 'fld1';
+		$form = new FormModel();
+		$field->setForm($form);
+		$field->setId($id);
+		$field->setDefault($default);
+		$field->setValue($value);
+		$array = $field->toArray();
+		$this->assertEquals( $id, $array['id' ] );
+		$this->assertEquals( $default, $array['default' ] );
+		$this->assertEquals( $value, $array['value' ] );
+		$this->assertEquals( $form, $array['form' ] );
+	}
+
+	public function testFromArray()
+	{
+		$default = 'drinks';
+		$value = 'food';
+		$id = 'fld1';
+		$field = FieldModel::fromArray(
+			[
+				'id' => $id,
+				'value' => $value,
+				'default' => $default
+			]
+		);
+		$this->assertEquals($id, $field->getId() );
+		$this->assertEquals($value, $field->getValue() );
+		$this->assertEquals($default, $field->getDefault() );
+
+	}
+
 
 }
