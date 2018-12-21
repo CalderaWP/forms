14ec0355172518befbdb3104a7b56c2b47a6c72d
Josh Pollock <josh@calderawp.com>
2018-12-21 15:37:27 -0500

    field options

---
+++ src/Field/FieldConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace calderawp\caldera\Forms\Field;
+
+
+class FieldConfig
+{
+
+}
---
+++ tests/FieldConfigTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace calderawp\caldera\Forms\Tests;
+
+use calderawp\caldera\Forms\Field\FieldConfig;
+
+class FieldConfigTest extends TestCase
+{
+
+}
